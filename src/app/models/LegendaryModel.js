a3f0655dc63c61073056cd460456387a4a99f31b
Lilian Melo <107283230+LilianLucena@users.noreply.github.com>
2022-09-09 08:12:25 -0300

    Add files via upload

--- src/app/models/LegendaryModel.js
+++ src/app/models/LegendaryModel.js
@@ -1,14 +1,34 @@
-function LegendaryModel(id, name, description, type, healthPoints, specialAttack, defense, attack, experience, specialDefense) {
-  this.id = id;
-  this.name = name;
-  this.description = description;
-  this.type = type;
-  this.healthPoints = healthPoints;
-  this.specialAttack = specialAttack;
-  this.defense = defense;
-  this.attack = attack;
-  this.experience = experience;
-  this.specialDefense = specialDefense
-}
+import Sequelize, { Model } from "sequelize";
+import databaseConfig from "../../../config/database";
+import LegendariesTrainers from "../LegendariesTrainers/LegendariesTrainers";
+import TrainerModel from "../trainer/TrainerModel";
 
-module.exports = LegendaryModel;
+const sequelize = new Sequelize(databaseConfig);
+
+class LegendaryModel extends Model {}
+LegendaryModel.init(
+  {
+    id: {
+      type: Sequelize.UUIDV4(),
+      primaryKey: true,
+    },
+    name: Sequelize.STRING,
+    description: Sequelize.STRING,
+    type: Sequelize.STRING,
+    healthPoints: Sequelize.INTEGER,
+    specialAttack: Sequelize.INTEGER,
+    defense: Sequelize.INTEGER,
+    attack: Sequelize.INTEGER,
+    experience: Sequelize.INTEGER,
+    specialDefense: Sequelize.INTEGER,
+  },
+  {
+    sequelize,
+    modelName: "legendaries",
+    timestamps: false,
+  }
+);
+
+LegendaryModel.belongsToMany(TrainerModel, { through: LegendariesTrainers });
+
+export default LegendaryModel;
